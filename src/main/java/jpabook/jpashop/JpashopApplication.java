79facc3efd6c4c6c342f048200910d20f7a32abb
심기준 <shimkijun@gmail.com>
2023-06-04 09:57:06 +0900

    View 환경설정

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -8,12 +8,6 @@ public class JpashopApplication {
 
 
     public static void main(String[] args) {
-
-        Hello hello = new Hello();
-        hello.setData("set");
-        String data = hello.getData();
-        System.out.println(data);
-
         SpringApplication.run(JpashopApplication.class, args);
     }
 
